a258ffa62ca9b2887636e28e237f05fa538d31d7
AleCampo86 <alejandrocampo_86@hotmail.com>
2020-12-14 15:51:42 -0300

    Dejar mensaje

---
+++ nuxt.config.js
@@ -0,0 +1,137 @@
+
+/*!
+
+=========================================================
+* Nuxt Argon Dashboard PRO - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/nuxt-argon-dashboard-pro
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+
+* Coded by www.creative-tim.com and www.binarcode.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+const pkg = require('./package')
+console.log('ENV', process.env.NODE_ENV)
+
+
+
+module.exports = {
+  mode: 'universal',
+  router: {
+    base: '/',
+    linkExactActiveClass: 'active'
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'DATAVIA',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt Argon Dashboard PRO - Premium Nuxt.js & Bootstrap 4 Dashboard' }
+    ],
+    link: [
+      {
+        rel: "stylesheet",
+        href:
+          "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+      },
+      { rel: 'icon', type: 'image/png', href: 'favicon.png' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700'},
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap'},
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.6.3/css/all.css', integrity: "sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/", crossorigin: "anonymous"}
+    ],
+    script: [
+      {
+        src: "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+        type: "text/javascript"
+      },
+      {
+        src:
+          "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js",
+        type: "text/javascript"
+      },
+      {
+        src:
+          "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js",
+        type: "text/javascript"
+      },
+
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    'assets/css/nucleo/css/nucleo.css',
+    'assets/sass/argon.scss'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~/plugins/dashboard/dashboard-plugin',
+    {src: '~/plugins/dashboard/full-calendar', ssr: false },
+    {src: '~/plugins/dashboard/world-map', ssr: false },
+    {src: '~/plugins/three.js' },
+
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: [
+      'vee-validate/dist/rules'
+    ],
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+
+    },
+    extractCSS: process.env.NODE_ENV === 'production',
+    babel: {
+      plugins: [
+        [
+          "component",
+          {
+            "libraryName": "element-ui",
+            "styleLibraryName": "theme-chalk"
+          }
+        ]
+      ]
+    }
+  }
+}
---
+++ pages/Netflix.vue
@@ -0,0 +1,353 @@
+<template>
+  <div class="container">
+    <a href="#"
+      ><img
+        src="~assets/datavia-assets/datavia-assets/logo/datavia_logo_w.svg"
+        alt=""
+        class="datavia-logo"
+    /></a>
+
+    <!-- peliculas principal -->
+    <div class="pelicula-principal">
+      <div class="contenedor">
+        <!-- <h3 class="titulo">Interestellar</h3> -->
+        <!-- <p class="descripcion">
+          Narra las aventuras de un grupo de exploradores que hacen uso de un
+          agujero de gusano recientemente descubierto para superar las
+          limitaciones de los viajes espaciales tripulados y vencer las inmensas
+          distancias que tiene un viaje interestelar.
+        </p> -->
+
+        <!-- <button role="button" class="boton"><i class="fas fa-play"></i>AI Sensing</button> -->
+        <!-- <button role="button" class="boton"><i class="fas fa-info-circle"></i>More info</button> -->
+      </div>
+    </div>
+    <img
+      src="~assets/datavia-assets/datavia-assets/icons/icon-camera-setup.svg"
+      class="slider-icon mx-3"
+    />
+    <img
+      src="~assets/datavia-assets/datavia-assets/icons/icon-ai-sensing.svg"
+      class="slider-icon mx-3"
+    />
+    <img
+      src="~assets/datavia-assets/datavia-assets/icons/icon-dashboard-report.svg"
+      class="slider-icon mx-3"
+    />
+
+    <!-- peliculas recomendadas -->
+    <div class="peliculas-recomendadas contenedor">
+      <div class="contenedor-titulo-controles text-center my-2">
+        <h3>Applications & Demos</h3>
+        <div class="indicadores"></div>
+      </div>
+
+      <div class="contenedor-principal">
+        <button role="button" id="flecha-izquierda" class="flecha-izquierda">
+          <i class="fas fa-angle-left"></i>
+        </button>
+
+        <div class="contenedor-carousel">
+          <div class="carousel">
+            <div class="pelicula">
+              <a href="#"
+                ><img src="~assets/images/Biometric-identification.png" alt=""
+              /></a>
+            </div>
+            <div class="pelicula">
+              <a href="#"
+                ><img src="~assets/images/face-recog2.jpg" alt=""
+              /></a>
+            </div>
+            <div class="pelicula">
+              <a href="#"
+                ><img src="~assets/images/face-recog1.jpg" alt=""
+              /></a>
+            </div>
+            <div class="pelicula">
+              <a href="#"
+                ><img src="~assets/images/face-recog3.jpg" alt=""
+              /></a>
+            </div>
+            <div class="pelicula">
+              <a href="#"
+                ><img
+                  src="~assets/images/facial-recognition-technologies-time-to-face-the-music_1500.jpg"
+                  alt=""
+              /></a>
+            </div>
+          </div>
+        </div>
+
+        <button role="button" id="flecha-derecha" class="flecha-derecha">
+          <i class="fas fa-angle-right"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "netflix",
+  mounted() {
+    const fila = document.querySelector(".contenedor-carousel");
+    const peliculas = document.querySelectorAll(".pelicula");
+
+    const flechaIzquierda = document.getElementById("flecha-izquierda");
+    const flechaDerecha = document.getElementById("flecha-derecha");
+
+    // ? ----- ----- Event Listener para la flecha derecha. ----- -----
+    flechaDerecha.addEventListener("click", () => {
+      fila.scrollLeft += fila.offsetWidth;
+
+      const indicadorActivo = document.querySelector(".indicadores .activo");
+      if (indicadorActivo.nextSibling) {
+        indicadorActivo.nextSibling.classList.add("activo");
+        indicadorActivo.classList.remove("activo");
+      }
+    });
+
+    // ? ----- ----- Event Listener para la flecha izquierda. ----- -----
+    flechaIzquierda.addEventListener("click", () => {
+      fila.scrollLeft -= fila.offsetWidth;
+
+      const indicadorActivo = document.querySelector(".indicadores .activo");
+      if (indicadorActivo.previousSibling) {
+        indicadorActivo.previousSibling.classList.add("activo");
+        indicadorActivo.classList.remove("activo");
+      }
+    });
+
+    // ? ----- ----- Paginacion ----- -----
+    const numeroPaginas = Math.ceil(peliculas.length / 5);
+    for (let i = 0; i < numeroPaginas; i++) {
+      const indicador = document.createElement("button");
+
+      if (i === 0) {
+        indicador.classList.add("activo");
+      }
+
+      document.querySelector(".indicadores").appendChild(indicador);
+      indicador.addEventListener("click", (e) => {
+        fila.scrollLeft = i * fila.offsetWidth;
+
+        document
+          .querySelector(".indicadores .activo")
+          .classList.remove("activo");
+        e.target.classList.add("activo");
+      });
+    }
+
+    // ? ----- ----- Hover ----- -----
+    peliculas.forEach((pelicula) => {
+      pelicula.addEventListener("mouseenter", (e) => {
+        const elemento = e.currentTarget;
+        setTimeout(() => {
+          peliculas.forEach((pelicula) => pelicula.classList.remove("hover"));
+          elemento.classList.add("hover");
+        }, 300);
+      });
+    });
+
+    fila.addEventListener("mouseleave", () => {
+      peliculas.forEach((pelicula) => pelicula.classList.remove("hover"));
+    });
+  },
+};
+</script>
+
+<style scoped>
+/* ---- ----- ----- Pelicula Principal ----- ----- ----- */
+.pelicula-principal {
+  font-size: 16px;
+  min-height: 30em;
+  color: #fff;
+  background: linear-gradient(rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 100%),
+    url(~assets/images/AI-background.jpg);
+  background-position: center center;
+  background-size: cover;
+  margin-top: -40px;
+  margin-bottom: 1.12em;
+  display: flex;
+  align-items: end;
+}
+
+.pelicula-principal .contenedor {
+  margin: 0 auto;
+  margin-bottom: 6.25em;
+}
+
+.pelicula-principal .titulo {
+  font-weight: 600;
+  font-size: 3.12em;
+  margin-bottom: 0.4em;
+}
+
+.pelicula-principal .descripcion {
+  font-weight: normal;
+  font-size: 1em;
+  line-height: 1.75em;
+  max-width: 50%;
+  margin-bottom: 1.25em;
+}
+
+.pelicula-principal .boton {
+  background: rgba(0, 0, 0, 0.5);
+  border: none;
+  border-radius: 0.31em;
+  padding: 0.93em 1.87em;
+  color: #fff;
+  margin-right: 1.25em;
+  cursor: pointer;
+  transition: 0.3s ease all;
+  font-size: 1.12em;
+}
+
+.pelicula-principal .boton:hover {
+  background: #fff;
+  color: #000;
+}
+
+.pelicula-principal .boton i {
+  margin-right: 1.25em;
+}
+
+/* ---- ----- ----- Contenedor Titulo y Controles ----- ----- ----- */
+.contenedor-titulo-controles {
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+}
+
+.contenedor-titulo-controles h3 {
+  color: #fff;
+  font-size: 30px;
+  margin: auto;
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.contenedor-titulo-controles .indicadores button {
+  background: #fff;
+  height: 3px;
+  width: 10px;
+  cursor: pointer;
+  border: none;
+  margin-right: 2px;
+}
+
+.contenedor-titulo-controles .indicadores button:hover,
+.contenedor-titulo-controles .indicadores button.activo {
+  background: red;
+}
+
+/* ---- ----- ----- Contenedor Principal y Flechas ----- ----- ----- */
+.peliculas-recomendadas {
+  margin-bottom: 70px;
+}
+
+.peliculas-recomendadas .contenedor-principal {
+  display: flex;
+  align-items: center;
+  position: relative;
+}
+
+.peliculas-recomendadas .contenedor-principal .flecha-izquierda,
+.peliculas-recomendadas .contenedor-principal .flecha-derecha {
+  position: absolute;
+  border: none;
+  background: rgba(0, 0, 0, 0.3);
+  font-size: 40px;
+  height: 50%;
+  top: calc(50% - 25%);
+  line-height: 40px;
+  width: 50px;
+  color: #fff;
+  cursor: pointer;
+  z-index: 500;
+  transition: 0.2s ease all;
+}
+
+.peliculas-recomendadas .contenedor-principal .flecha-izquierda:hover,
+.peliculas-recomendadas .contenedor-principal .flecha-derecha:hover {
+  background: rgba(0, 0, 0, 0.9);
+}
+
+.peliculas-recomendadas .contenedor-principal .flecha-izquierda {
+  left: 0;
+}
+
+.peliculas-recomendadas .contenedor-principal .flecha-derecha {
+  right: 0;
+}
+
+/* ---- ----- ----- Carousel ----- ----- ----- */
+.peliculas-recomendadas .contenedor-carousel {
+  width: 100%;
+  padding: 20px 0;
+  overflow: hidden;
+  scroll-behavior: smooth;
+}
+
+.peliculas-recomendadas .contenedor-carousel .carousel {
+  display: flex;
+  flex-wrap: nowrap;
+}
+
+.peliculas-recomendadas .contenedor-carousel .carousel .pelicula {
+  min-width: 20%;
+  transition: 0.3s ease all;
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+}
+
+.peliculas-recomendadas .contenedor-carousel .carousel .pelicula.hover {
+  transform: scale(1.2);
+  transform-origin: center;
+  object-fit: cover;
+}
+
+.peliculas-recomendadas .contenedor-carousel .carousel .pelicula img {
+  width: 100%;
+  vertical-align: top;
+  object-fit: cover;
+  height: 100%;
+}
+
+/* ---- ----- ----- Media Queries ----- ----- ----- */
+@media screen and (max-width: 800px) {
+  header .logotipo {
+    margin-bottom: 10px;
+    font-size: 30px;
+  }
+
+  header .contenedor {
+    flex-direction: column;
+    text-align: center;
+  }
+
+  .pelicula-principal {
+    font-size: 14px;
+  }
+
+  .pelicula-principal .descripcion {
+    max-width: 100%;
+  }
+
+  .peliculas-recomendadas .contenedor-carousel {
+    overflow: visible;
+  }
+
+  .peliculas-recomendadas .contenedor-carousel .carousel {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+  }
+
+  .peliculas-recomendadas .indicadores,
+  .peliculas-recomendadas .flecha-izquierda,
+  .peliculas-recomendadas .flecha-derecha {
+    display: none;
+  }
+}
+</style>
