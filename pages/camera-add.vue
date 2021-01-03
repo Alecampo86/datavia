9c1a527b1a7eff9ef4b352a3476f3951a4b53a28
AleCampo86 <alejandrocampo_86@hotmail.com>
2021-01-03 13:55:44 -0300

    flotados ok

--- pages/camera-add.vue
+++ pages/camera-add.vue
@@ -21,7 +21,6 @@
           </div>
         </div>
       </nuxt-link>
-
       <!-- COLUMNA 1 -->
       <div class="columna1 col col-lg-6 h-100 w-100">
         <div class="column-title w-75">
@@ -32,42 +31,44 @@
           <h2 class="text-white mt-1">Adding a New Camera</h2>
         </div>
 
-        <nuxt-link to="/camera-A1">
-          <input
-            type="text"
-            class="camera-name form-control mt-3 w-75 p-2"
-            placeholder="Camera Name"
-          />
-        </nuxt-link>
+
+          <div class="w-75">
+            <nuxt-link to="/camera-A1">
+              <input
+                type="text"
+                placeholder="Camera A1 - Building R1 - Floor 1 A"
+                class="form-control mt-3  p-2 bg-dark"
+              />
+            </nuxt-link>
+          </div>
+
+        <!-- <span>Camera A1 - Building R1 - Floor 1 A</span> -->
+
         <div class="rowSelect row w-100">
           <input
             type="select"
             class="select1 form-control bg-dark mt-3 col-4 p-2"
-            placeholder="Select Camera Brand"
+            placeholder="Sony"
           />
           <input
             type="select"
             class="select2 form-control bg-dark mt-3 col-4 p-2"
-            placeholder="Select Camera Model"
+            placeholder="ABC100"
           />
         </div>
 
         <div class="columnaBox w-75 h-25 mt-5">
-          <img
-            class="box-icon-cam"
-            src="~assets/datavia-assets/datavia-assets/icons/icon-camera.svg"
-          />
+          <img src="~assets/datavia-assets/datavia-assets/icons/icon-camera.svg" />
         </div>
 
-        <input
-          type="textarea"
-          placeholder="Description"
-          class="columnaBox2 form-control bg-dark w-75 h-25 mt-3"
-        />
+        <div class="form-group">
+          <textarea class="cam-description bg-dark form-control p-2" rows="3">Descripción
+</textarea
+          >
+        </div>
       </div>
 
       <!-- COLUMNA 2 -->
-
       <div class="columna2 col col-lg-6 h-100 w-100">
         <div class="column-title w-75">
           <img
@@ -84,29 +85,27 @@
           placeholder="URL"
         />
 
+
         <div class="columnaBox3 w-75 mt-5">
-          <img
-            class="box-icon-cam"
-            src="~assets/datavia-assets/datavia-assets/icons/icon-camera.svg"
-          />
+          <img src="~assets/datavia-assets/datavia-assets/icons/icon-camera.svg"/>
         </div>
 
         <div>
           <button class="btn cancel-btn">
             <i class="far fa-window-close"></i>
-            <span>Cancel</span>
+            Cancel
           </button>
         </div>
 
         <div class="save-continue">
-          <button class="btn save-btn" disabled>
-            <img
-              class="icon-save"
-              src="~assets/datavia-assets/datavia-assets/icons/icon-save.svg"
-            />
-
-            <span>Save and Continue</span>
-          </button>
+          <nuxt-link to="/camera-save">
+            <button class="btn save-btn">
+              <img
+                class="icon-save"
+                src="~assets/datavia-assets/datavia-assets/icons/icon-save.svg"
+              />Save and Continue
+            </button>
+          </nuxt-link>
         </div>
       </div>
     </div>
@@ -114,13 +113,7 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      boton: true,
-    };
-  },
-};
+export default {};
 </script>
 
 <style scoped>
@@ -129,6 +122,11 @@ export default {
   /* height: 100vh; */
   margin: 0px;
   padding: 0px;
+}
+
+a {
+  width: 100%;
+  text-align: -webkit-center;
 }
 
 .cancel-btn {
@@ -172,21 +170,7 @@ export default {
   font-size: 12px;
 }
 
-a {
-  width: 100%;
-  text-align: -webkit-center;
-}
-
-.camera-name {
-  background: #212529;
-  transition: all 200ms;
-}
-
-.camera-name:hover {
-  background: #408388;
-}
-
-.container[data-v-48ca2ab2] {
+.container {
   width: 80%;
   height: 630px;
   margin: 0 auto;
@@ -209,18 +193,32 @@ a {
   border-color: rgb(100, 100, 100);
   cursor: pointer;
 }
-
+.cam-description {
+  position: absolute;
+  bottom: 119px;
+  left: 73px;
+  width: 454px;
+  height: 140px;
+}
 .form-control:focus {
   border-color: #85b0cb;
   box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px #85b0cb;
 }
 
-.form-control:hover {
-}
-
 #background {
   background: #191a1d;
   height: 48em;
+}
+
+.icon-menu {
+  padding: 0;
+  cursor: pointer;
+  background: #85b0cb;
+  border-radius: 10px;
+  position: absolute;
+  top: -86px;
+  left: 0px;
+  transition: all 200ms;
 }
 
 .column-title {
@@ -235,23 +233,6 @@ a {
   position: absolute;
   left: 82px;
   top: 4px;
-}
-
-.icon-menu {
-  padding: 0;
-  cursor: pointer;
-  background: #85b0cb;
-  border-radius: 7px;
-  position: absolute;
-  width: 37px;
-  top: -86px;
-  left: 0px;
-  -webkit-transition: all 200ms;
-  transition: all 200ms;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 38px;
 }
 
 .icon-menu small {
@@ -285,22 +266,18 @@ a {
 }
 
 .box-3 {
-  display: -webkit-box;
   display: flex;
-  -webkit-box-align: baseline;
-  align-items: baseline;
-  width: 285px;
-  height: 135px;
+
+  align-items: center;
+  width: 300px;
+  height: 90px;
   top: 35px;
-  left: 639px;
+  left: 606px;
   position: absolute;
 }
 
 .box-3 .box-icon-cam {
-  width: 21%;
-  position: absolute;
-  top: -10px;
-  left: -65px;
+  width: 20%;
 }
 
 .box-3 h3 {
@@ -315,7 +292,7 @@ a {
 .columna1 {
   display: flex;
   align-items: center;
-  justify-content: flex-start;
+  justify-content:flex-start;
   flex-flow: column wrap;
 }
 
@@ -358,6 +335,14 @@ a {
   top: 336px;
 }
 
+.icon-success {
+  color: greenyellow;
+  font-size: 25px;
+  position: absolute;
+  right: 90px;
+  top: 54px;
+}
+
 .columnaBox3 {
   display: flex;
   align-items: center;
@@ -369,5 +354,12 @@ a {
   border: solid #85b0cb;
   border-radius: 10px;
   cursor: pointer;
+  object-fit: cover;
+}
+
+.columnaBox3 img {
+
+  object-fit: initial;
+
 }
 </style>
